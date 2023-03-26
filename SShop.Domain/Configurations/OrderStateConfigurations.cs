84638b3afa8082c9b05a8a96c679ff843f5cc0c6
MinhSon <nguyenminhson102002@gmail.com>
2023-03-26 19:27:03 +0700

    add entity OrderState + config

---
+++ SShop.Domain/Configurations/OrderStateConfigurations.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using SShop.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SShop.Domain.Configurations
+{
+    public class OrderStateConfigurations : IEntityTypeConfiguration<OrderState>
+    {
+        public void Configure(EntityTypeBuilder<OrderState> builder)
+        {
+            builder.ToTable(nameof(OrderState));
+
+            builder.HasKey(x => x.OrderStateId);
+            builder
+                .Property(x => x.OrderStateId)
+                .UseIdentityColumn();
+
+            builder
+                .Property(x => x.OrderStateName)
+                .HasMaxLength(100)
+                .IsRequired();
+
+            builder
+                .HasMany(x => x.Orders)
+                .WithOne(x => x.OrderState)
+                .HasForeignKey(x => x.OrderStateId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
---
+++ SShop.Domain/Entities/OrderState.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SShop.Domain.Entities
+{
+    public class OrderState
+    {
+        public int OrderStateId { get; set; }
+        public int OrderStateName { get; set; }
+        public HashSet<Order> Orders { get; set; }
+    }
+}
